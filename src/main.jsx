24708162d12c8c3338879f94c8a3d8124dbd799e
abu bakar <abubakar850772@gmail.com>
2024-08-25 21:41:45 +0600

    added authContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {
-  RouterProvider,
-} from "react-router-dom";
-import router from './Routers/Routers.jsx';
-import { ThemeProvider } from '@material-tailwind/react';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routers/Routers.jsx";
+import { ThemeProvider } from "@material-tailwind/react";
+import AuthProvider from "./AuthProvider/AuthProvider.jsx";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider>
-    <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
