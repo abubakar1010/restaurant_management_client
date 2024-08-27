b3099e2612aafd2c6866637c4d1d3de2d3e445e3
abu bakar <abubakar850772@gmail.com>
2024-08-27 09:11:22 +0600

    finished error page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -4,12 +4,18 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
-        <div>
-            <p>oops error</p>
-            <Link to={"/"}>
-            <Button>Back to home</Button>
-            </Link>
-        </div>
+        <>
+        <div className=" bg-[url('https://i.postimg.cc/kMfg17Jj/21586054-Na-Nov-26.jpg')] w-full h-screen bg-cover bg-center bg-no-repeat relative ">
+            
+                <div className=" w-full text-center pb-12 flex justify-center items-end h-full">
+                  <Link to={"/"}>
+                    <button className="  px-7 py-2 rounded-sm font-bold text-white text-xl bg-gradient-to-r from-[#d0a260] to-[#c79c60da]">Go Home</button>
+                  </Link>
+                </div>
+              
+            </div>
+          
+        </>
     );
 };
 
