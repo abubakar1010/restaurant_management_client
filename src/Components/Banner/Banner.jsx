5f88252828f569539331bc51613fe3a632855028
abu bakar <abubakar850772@gmail.com>
2024-08-27 11:03:20 +0600

    update banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -29,10 +29,10 @@ const Banner = () => {
           <div className=" bg-[url('https://i.postimg.cc/3Jdrs5PW/pexels-pixabay-260922.jpg')] w-ful h-[520px] bg-cover bg-center bg-no-repeat my-8 rounded-xl relative ">
             <div className="absolute inset-0 grid h-full w-full bg-[#1d1d1d70] rounded-xl">
               <div className="text-white pt-28">
-                <h1 className=" text-6xl font-extrabold text-center ">
+                <h1 className=" text-3xl lg:text-5xl lg:font-bold text-center">
                 Our menu brings a taste of the vibrant dishes{" "}
                 </h1>
-                <p className=" text-2xl uppercase font-bold mt-7 mb-3 text-center">
+                <p className="lg:text-2xl uppercase lg:font-medium mt-7 mb-3 text-center">
                   <Typewriter
                     words={[
                       "Let us create an unforgettable experience",
@@ -46,7 +46,7 @@ const Banner = () => {
                     delaySpeed={1000}
                   />
                 </p>
-                <div className=" w-full text-center mt-12">
+                <div className=" w-full text-center lg:mt-12">
                   <Link to={"/allFoods"}>
                     <button className=" mt-6 px-7 py-2 rounded-sm font-bold text-white text-xl bg-gradient-to-r from-[#d0a260] to-[#c79c60da]">All Foods</button>
                   </Link>
@@ -60,13 +60,13 @@ const Banner = () => {
           <div className=" bg-[url('https://i.postimg.cc/3J0MYK94/pexels-igor-starkov-233202-1237073.jpg')] w-full h-[520px] bg-cover bg-center bg-no-repeat my-8 rounded-xl relative ">
             <div className="absolute inset-0 grid h-full w-full bg-[#1d1d1d70] rounded-xl">
               <div className="text-white  pt-28">
-                <h1 className=" text-6xl font-extrabold text-center ">
+                <h1 className=" text-3xl lg:text-5xl lg:font-bold text-center">
                 Enjoy a tantalising seasonal brunch menu.{" "}
                 </h1>
-                <p className=" text-2xl uppercase font-bold mt-7 mb-3 text-center">
+                <p className="lg:text-2xl uppercase lg:font-medium mt-7 mb-3 text-center">
                 We are delighted to welcome you to our restaurant for an extraordinary culinary experience
                 </p>
-                <div className=" w-full text-center mt-12">
+                <div className=" w-full text-center lg:mt-12">
                   <Link to={"/allFoods"}>
                     <button className=" mt-6 px-7 py-2 rounded-sm font-bold text-white text-xl bg-gradient-to-r from-[#d0a260] to-[#c79c60da]">All Foods</button>
                   </Link>
@@ -80,13 +80,13 @@ const Banner = () => {
           <div className=" bg-[url('https://i.postimg.cc/J0qLXKDy/pexels-quark-studio-1159039-3201921.jpg')] w-full h-[520px] bg-cover bg-center bg-no-repeat my-8 rounded-xl relative ">
             <div className="absolute inset-0 grid h-full w-full bg-[#1d1d1d70] rounded-xl">
               <div className="text-white  pt-28">
-                <h1 className=" text-6xl font-extrabold text-center ">
+                <h1 className=" text-3xl lg:text-5xl lg:font-bold text-center">
                   The Service is worth seeing{" "}
                 </h1>
-                <p className=" text-2xl uppercase font-bold mt-7 mb-3 text-center">
+                <p className="lg:text-2xl uppercase lg:font-medium mt-7 mb-3 text-center">
                 Our team welcomes you with a smile for a memorable dining experience.
                 </p>
-                <div className=" w-full text-center mt-12">
+                <div className=" w-full text-center lg:mt-12">
                   <Link to={"/allFoods"}>
                     <button className=" mt-6 px-7 py-2 rounded-sm font-bold text-white text-xl bg-gradient-to-r from-[#d0a260] to-[#c79c60da]">All Foods</button>
                   </Link>
--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,13 +5,14 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAsO6vszQqR5umgSVHEVQlcJ0-0RKdBQUA",
-  authDomain: "restaurant-management-89e37.firebaseapp.com",
-  projectId: "restaurant-management-89e37",
-  storageBucket: "restaurant-management-89e37.appspot.com",
-  messagingSenderId: "1093713112363",
-  appId: "1:1093713112363:web:274625eed70fb7b35ba96e"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
